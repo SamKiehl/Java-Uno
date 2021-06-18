bbcbcd4c80fc8682b3886844d82492891b772fbc
Sam Kiehl <e22-kiehlsg@elderhs.net>
2021-06-18 16:13:10 -0400

    Updated functionality of play1P method, fixed a bug where UnoCard objects with a face value of 0 would not display any face value through the toString.

--- UnoCard.java
+++ UnoCard.java
@@ -1,4 +1,4 @@
-public class UnoCard {
+public class UnoCard implements Comparable<UnoCard>{
     private String color;
     private int face; /* 1 - 9 nums, 10: skip, 11: reverse, 12: +2, 13: Wild, 14: +4 Wild */
 
@@ -11,6 +11,18 @@ public class UnoCard {
 
     public int getFace(){return this.face;}
 
+    public int compareTo(UnoCard other){
+        if(this.face == other.face && this.color.equals(other.color))
+            return 0;
+        return -1;
+    }
+
+    public boolean equals(UnoCard other){
+        if(this.compareTo(other) == 0)
+            return true;
+        return false;
+    }
+
     public String toString(){
         String faceAdd = "";
         if(face == 13)
@@ -19,11 +31,12 @@ public class UnoCard {
             return "+4 Wild!";
         else if(face == 10)
             faceAdd = "Skip";
+            
         else if(face == 11)
             faceAdd = "Reverse";
         else if(face == 12)
             faceAdd = "+2";
-        else if(face < 1 || face > 14)
+        else if(face < 0 || face > 14)
             faceAdd = "";
         else
             faceAdd = face + "";
